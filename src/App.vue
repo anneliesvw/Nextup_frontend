bf56b1f65bc77ade983d885ddc84b1204e30d66f
Mahen Vermeulen <mahenvermeulen@msn.com>
2018-01-31 12:16:54 +0100

    header toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="stretch-height">
     <div class="app-header">
-      <p>MATTHIAS IS LELIJK</p>
+      <div class="title">Next Up</div>
+      <div class="links">
+        <ol>
+          <li>My Activities</li>
+          <li>Discover Activities</li>
+          <li class="active-link">My Groups</li>
+        </ol>
+      </div>
+      <div class="user-info">
+        <div class="avatar"></div>
+        <div class="name">Matthias</div>
+      </div>
     </div>
     <router-view></router-view>
   </div>
