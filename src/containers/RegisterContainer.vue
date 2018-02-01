cb240f0065d15e26893487c87d637bde000ac2d1
Matthias Goossens <matthias.goossens@hotmail.com>
2018-02-01 21:56:41 +0100

    update registratie/login

--- src/containers/RegisterContainer.vue
+++ src/containers/RegisterContainer.vue
@@ -2,16 +2,17 @@
   <div class="site-wrapper">
     <div class="center-content">
       <div class="register-form">
-        <!-- <div class="circle-logo">
-          <img src="http://thedisneyblog.com/wp-content/uploads/2012/04/n-logo-big.png">
-        </div> -->
         <el-tabs value="first">
           <el-tab-pane label="Sign in" name="first">
             <el-form label-position="top">
-              <el-form-item class="form-item" label="Username">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-user fa-lg"></i> Username</span>
                 <el-input></el-input>
               </el-form-item>
-              <el-form-item class="form-item" label="Password">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-paper-plane fa-lg"></i> Password</span>
                 <el-input></el-input>
               </el-form-item>
               <el-form-item class="form-button">
@@ -19,21 +20,30 @@
               </el-form-item>
             </el-form>
           </el-tab-pane>
+
           <el-tab-pane label="Join us" name="second">
             <div class="register-text">
               Start planning today!
             </div>
             <el-form label-position="top">
-              <el-form-item class="form-item" label="E-mail">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-envelope fa-lg"></i> E-mail</span>
                 <el-input></el-input>
               </el-form-item>
-              <el-form-item class="form-item" label="Username">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-user fa-lg"></i> Username</span>
                 <el-input></el-input>
               </el-form-item>
-              <el-form-item class="form-item" label="Password">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-paper-plane fa-lg"></i> Password</span>
                 <el-input></el-input>
               </el-form-item>
-              <el-form-item class="form-item" label="Confirm Password">
+              <el-form-item class="form-item">
+                <span slot="label">
+                  <i class="far fa-paper-plane fa-lg"></i> Confirm Password</span>
                 <el-input></el-input>
               </el-form-item>
               <el-form-item class="form-button">
@@ -42,13 +52,24 @@
             </el-form>
           </el-tab-pane>
         </el-tabs>
-
-
-        
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  export default {
+    name: 'RegisterContainer',
+    data() {
+      return {
+        selectedTab: 'login',
+      };
+    },
+    methods: {
+      changeTab(event) {
+        this.selectedTab = event;
+        window.console.log(this.selectedTab);
+      },
+    },
+  };
 </script>
