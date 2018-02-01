29a71ac593b0ac7b58bdcb465d0e3c3cd4d8475f
Matthias Goossens <matthias.goossens@hotmail.com>
2018-02-01 15:26:04 +0100

    User settings dropdown added

--- src/containers/MainContainer.vue
+++ src/containers/MainContainer.vue
@@ -1,16 +1,20 @@
 <template>
+  <div class="site-wrapper">
+    <Navigation></Navigation>
     <div class="content-container">
       <Activity v-for="item in 10" :key="item"></Activity>
-      
     </div>
+  </div>
 </template>
 
 <script>
-import Activity from '../components/activities/Activity.vue';
+  import Activity from '../components/activities/Activity.vue';
+  import Navigation from '../components/header/Navigation.vue';
 
-export default {
-  components: {
-    Activity,
-  },
-};
+  export default {
+    components: {
+      Activity,
+      Navigation,
+    },
+  };
 </script>
