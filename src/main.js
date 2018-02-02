95da3e3ec8f35582d5082fb13d78dec9bb58bac1
Unknown <rb.bogaerts@gmail.com>
2018-02-02 15:50:46 +0100

    Bugfixes (matthias zijne rommel)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
 import Vue from 'vue';
 import router from './router';
 import store from './store/store';
 import App from './App.vue';
 import './styles/index.scss';
+
+Vue.use(ElementUI);
 
 /* eslint-disable */
 const app = new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import Main from '../containers/MainContainer.vue';
 import Register from '../containers/RegisterContainer.vue';
 
 Vue.use(Router);
-Vue.use(ElementUI);
 
 export default new Router({
   routes: [
