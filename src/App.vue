76c874128d6827c16dd22b68891533801428a007
Matthias Goossens <matthias.goossens@hotmail.com>
2018-02-02 16:32:44 +0100

    Navigation terug in App.Vue gezet + routing aangepast

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="site-wrapper">
+    <Navigation v-if="loggedIn"></Navigation>
     <router-view></router-view>
   </div>
-  
+
 </template>
 
 <script>
+  import Navigation from './components/header/Navigation.vue';
+
+  export default {
+    data() {
+      return {
+        loggedIn: false,
+      };
+    },
+    components: {
+      Navigation,
+    },
+    beforeCreate() {
+      const token = localStorage.getItem('NEXTUP_TOKEN');
+      if (token != null) {
+        this.loggedIn = true;
+      } else {
+        this.$router.push('Register');
+      }
+    },
+  };
 </script>
