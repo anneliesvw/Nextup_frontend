c23aebddcd3446725a17db090f16af6139dfa0bb
Robbe <rb.bogaerts@gmail.com>
2018-02-03 12:13:15 +0100

    Added login feedback (UI)

--- src/App.vue
+++ src/App.vue
@@ -7,54 +7,58 @@
 </template>
 
 <script>
-  import LoginEvents from './events/loginevents';
-  import Navigation from './components/header/Navigation.vue';
-  import AuthService from './services/authservice';
+import { mapActions } from 'vuex';
+import LoginEvents from './events/loginevents';
+import Navigation from './components/header/Navigation.vue';
+import AuthService from './services/authservice';
 
-  export default {
-    data() {
-      return {
-        loggedIn: false,
-      };
-    },
-    components: {
-      Navigation,
-    },
-    methods: {
-      tryLogin(loginInfo) {
-        window.console.log(loginInfo);
-        AuthService.tryLogin(
-          loginInfo.username,
-          loginInfo.password,
-          (d) => {
-            localStorage.setItem('NEXTUP_TOKEN', d.data.access_token);
-            this.$router.push('/');
-            this.loggedIn = true;
-          },
-          (e) => {
-            window.console.log('failed to login.', e);
-          },
-        );
-      },
-      checkToken() {
-        const token = localStorage.getItem('NEXTUP_TOKEN');
-        window.console.log(token);
-        if (token != null) {
+export default {
+  data() {
+    return {
+      loggedIn: false,
+    };
+  },
+  components: {
+    Navigation,
+  },
+  methods: {
+    ...mapActions(['setLoginAttempt']),
+    tryLogin(loginInfo) {
+      window.console.log(loginInfo);
+      AuthService.tryLogin(
+        loginInfo.username,
+        loginInfo.password,
+        d => {
+          localStorage.setItem('NEXTUP_TOKEN', d.data.access_token);
+          this.$router.push('/');
           this.loggedIn = true;
-        } else {
-          this.$router.push('Register');
-        }
-      },
+          this.setLoginAttempt('success');
+        },
+        e => {
+          window.console.log('failed to login.', e);
+          this.setLoginAttempt('failed');
+        },
+      );
     },
-    beforeCreate() {
-      // this.checkToken();
+    checkToken() {
+      const token = localStorage.getItem('NEXTUP_TOKEN');
+      window.console.log(token);
+      if (token != null) {
+        this.loggedIn = true;
+      } else {
+        this.$router.push('Register');
+      }
     },
-    mounted() {
-      this.checkToken();
-      LoginEvents.bus.$on(LoginEvents.TRY_LOGIN, this.tryLogin);
-    },
-    beforeDestroy() {
-      LoginEvents.bus.$off(LoginEvents.TRY_LOGIN, this.tryLogin);
-    },
-  };
+  },
+  beforeCreate() {
+    // this.checkToken();
+  },
+  mounted() {
+    this.checkToken();
+    LoginEvents.bus.$on(LoginEvents.TRY_LOGIN, this.tryLogin);
+  },
+  beforeDestroy() {
+    LoginEvents.bus.$off(LoginEvents.TRY_LOGIN, this.tryLogin);
+  },
+};
 </script>
