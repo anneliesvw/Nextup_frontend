8f3791467432eefe1fcf6d537ae5a079f704062b
Mahen Vermeulen <mahenvermeulen@msn.com>
2018-02-04 21:58:38 +0100

    moved some components + message component

---
+++ src/containers/users/RegisterContainer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="site-wrapper">
+    <div class="center-content">
+      <div class="register-form">
+        <el-tabs value="first">
+          <el-tab-pane class="tab-content" label="Sign in" name="first">
+            <div class="register-text">
+              Login, new events ahead!
+            </div>
+            <el-form label-position="top">
+              <el-form-item label="Username" class="form-item">
+                <el-input v-model="loginInfo.username" @change="setLoginAttempt('')"></el-input>
+              </el-form-item>
+              <el-form-item label="Password" class="form-item">
+                <el-input v-model="loginInfo.password" type="password" @change="setLoginAttempt('')"></el-input>
+              </el-form-item>
+              <el-alert v-if="getLoginAttempt" title="Wrong password" type="error">
+              </el-alert>
+              <el-form-item class="form-button">
+                <el-button type="primary" @click="onLogin()">Login</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+
+          <el-tab-pane class="tab-content" label="Join us" name="second">
+            <div class="register-text">
+              Start planning today!
+            </div>
+            <el-form label-position="top">
+              <el-form-item label="E-mail" class="form-item">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="Firstname" class="form-item">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="Lastname" class="form-item">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="Password" class="form-item">
+                <el-input type="password"></el-input>
+              </el-form-item>
+              <el-form-item label="Confirm Password" class="form-item">
+                <el-input type="password"></el-input>
+              </el-form-item>
+              <el-form-item class="form-button">
+                <el-button type="primary">Register</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import LoginEvents from '../../events/loginevents';
+
+export default {
+  name: 'RegisterContainer',
+  data() {
+    return {
+      loginInfo: {
+        username: '',
+        password: '',
+      },
+      selectedTab: 'login',
+    };
+  },
+  computed: {
+    ...mapGetters(['getLoginAttempt']),
+  },
+  methods: {
+    ...mapActions(['setLoginAttempt']),
+    changeTab(event) {
+      this.selectedTab = event;
+      window.console.log(this.selectedTab);
+    },
+    onLogin() {
+      LoginEvents.bus.$emit(LoginEvents.TRY_LOGIN, this.loginInfo);
+    },
+  },
+};
+</script>
