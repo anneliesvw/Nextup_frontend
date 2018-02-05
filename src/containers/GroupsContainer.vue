656a2691a88717a20b28f139dfef37e1398cc2a3
Matthias Goossens <matthias.goossens@hotmail.com>
2018-02-05 13:17:18 +0100

    empty state title font

--- src/containers/GroupsContainer.vue
+++ src/containers/GroupsContainer.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="groups-container">
       <!--Sidebar></Sidebar-->
-      <!-- <div class="content-container">
+      <div class="content-container">
         <div class="group-wrapper">
           <div class="group-add">
             <i class="fas fa-plus"></i>
@@ -10,13 +10,7 @@
           </div>
         </div>
         <Group v-for="item in 5" :key="item"></Group>
-      </div> -->
-      <empty-state 
-        size="large"
-        icon="calendar-times"
-        title="You don't have any activities planned"
-        text="If you attend activities from your groups they will be shown here. You can also import your own agenda to show other activities you have planned. ">
-        </empty-state>
+      </div>
     </div>
   </div>
 </template>
@@ -24,13 +18,11 @@
 <script>
 import Group from '../components/groups/Group.vue';
 import Sidebar from '../components/sidebar/Sidebar.vue';
-import EmptyState from '../components/emptystate/EmptyState.vue';
 
 export default {
   components: {
     Group,
     Sidebar,
-    EmptyState,
   },
 };
 </script>
