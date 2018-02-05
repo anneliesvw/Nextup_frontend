3ccd4ed3cd51de3cc9b672cea4b6937cb0453475
Annelies <annelies.vanwallendael@student.kdg.be>
2018-02-05 11:51:16 +0100

    Aanmaken van CreateGroup page
    Dialog opent, maar nog een paar probleempjes met de maxwidth...

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,23 @@ import Router from 'vue-router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Main from '../containers/MainContainer.vue';
+import CreateGroup from '../containers/CreateGroup.vue';
 
 Vue.use(Router);
 Vue.use(ElementUI);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Default',
       component: Main,
+    },
+    {
+      path: '/group/create',
+      name: 'CreateGroup',
+      component: CreateGroup,
     },
   ],
 });
