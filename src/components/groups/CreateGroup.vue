0f7b773470e44861e6f24563c9559ebc8eb511fd
Annelies <annelies.vanwallendael@student.kdg.be>
2018-02-06 14:19:30 +0100

    Create-group-CSS-aanpassingen
    Naamgeving is aangepast en alles wat onnodig was, is eruit.

--- src/components/groups/CreateGroup.vue
+++ src/components/groups/CreateGroup.vue
@@ -1,19 +1,19 @@
 <template>
   <el-dialog title="Create group" class="createGroup-dialog" 
     :visible.sync="dialogVisible">
-    <div class="createGroup-form">
+    <div class="create-group-form">
       <el-form label-position="top" class="createGroup-name">
-        <div class="createGroup-graphic">
-          <div class="createGroup-cirkel">
+        <div class="create-group-graphic">
+          <div class="create-group-cirkel">
             <i class="fas fa-camera camera-icon"></i>
           </div>
         </div>
-        <div class="createGroup-left">
+        <div class="create-group-left">
             <el-form-item label="Name">
-              <el-input class="createGroup-input"></el-input>
+              <el-input class="create-group-input"></el-input>
             </el-form-item>
             <el-form-item label="Description">
-              <el-input class="createGroup-input" type="textarea" :autosize="{minRows: 4, maxRows: 5}"></el-input>
+              <el-input class="create-group-input" type="textarea" :autosize="{minRows: 4, maxRows: 5}"></el-input>
             </el-form-item>
         </div>
       </el-form>
@@ -26,63 +26,19 @@
 </template>
 
 <script>
-  export default {
-    props: ['isVisible'],
-    data() {
-      return {
-        friends: [],
-        members: [],
-        memberToAdd: '',
-      };
-    },
-    computed: {
-      dialogVisible: {
-        get() {
-          return this.isVisible;
-        },
-        set(newValue) {
-          if (!newValue) {
-            this.$emit('close');
-          }
-        },
+export default {
+  props: ['isVisible'],
+  computed: {
+    dialogVisible: {
+      get() {
+        return this.isVisible;
+      },
+      set(newValue) {
+        if (!newValue) {
+          this.$emit('close');
+        }
       },
     },
-    methods: {
-      addMember() {
-        this.members.push({
-          name: this.memberToAdd,
-        });
-        this.memberToAdd = '';
-      },
-      removeMember(index) {
-        // TODO: remove right
-        this.members.splice(index, 1);
-      },
-      querySearch(queryString, cb) {
-        const results = queryString ?
-          this.friends.filter(this.createFilter(queryString)) :
-          this.friends;
-        // call callback function to return suggestions
-        cb(results);
-      },
-      createFilter(queryString) {
-        return friend =>
-          friend.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0;
-      },
-      loadAll() {
-        return [{
-          value: 'Robbe12',
-        }, {
-          value: 'Matjas',
-        }, {
-          value: 'Mahen',
-        }, {
-          value: 'Annelies',
-        }];
-      },
-    },
-    mounted() {
-      this.friends = this.loadAll();
-    },
-  };
+  },
+};
 </script>
