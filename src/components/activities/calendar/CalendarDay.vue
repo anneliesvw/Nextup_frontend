3da94c7092a84e0b8582fa34978bdfd7fe2ea218
Robbe <rb.bogaerts@gmail.com>
2018-02-20 12:05:00 +0100

    laptop switch commit

--- src/components/activities/calendar/CalendarDay.vue
+++ src/components/activities/calendar/CalendarDay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="day-of-week">
-    <div class="day-header">{{ formatDate(data) }}</div>
+    <!-- <div class="day-header">{{ formatDate(data) }}</div> -->
     <div class="day-content">
       <div class="day-hour" v-for="n in 24" :key="n">
         <!-- <div class="day-quarter-hour" v-for="m in 4" :key="m"></div> -->
--- src/components/activities/calendar/CalendarList.vue
+++ src/components/activities/calendar/CalendarList.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="max-width calendar-wrapper">
-    <div class="calendar-hours">
-      <div class="calendar-hour" v-for="n in 24" :key="n">{{ n }}:00</div>
+    <div class="day-headers-wrapper">
+      <div class="day-header" v-for="day in getDaysOfThisWeek()" :key="day.toUTCString()">{{ formatDate(day) }}</div>
     </div>
-    <div class="days-wrapper">
-      <calendar-day v-for="day in getDaysOfThisWeek()" :key="day.toUTCString()" :data="day"></calendar-day>
+
+    <div class="scrollable-content" ref="scrollablecontent">
+
+      <div class="calendar-hours">
+        <div class="calendar-hour" v-for="n in 24" :key="n">{{ n }}:00</div>
+      </div>
+
+      <div class="days-wrapper">
+        <calendar-day v-for="day in getDaysOfThisWeek()" :key="day.toUTCString()" :data="day"></calendar-day>
+      </div>
+
     </div>
+
     <!-- <el-select v-model="value" placeholder="Select">
       <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
       </el-option>
@@ -14,50 +24,56 @@
 </template>
 
 <script>
-import CalendarDay from './CalendarDay.vue';
+  import CalendarDay from './CalendarDay.vue';
 
-export default {
-  components: {
-    CalendarDay,
-  },
-  data() {
-    return {
-      test: 'fdfd',
-    };
-  },
-  methods: {
-    formatDate(dateIn) {
-      return `${dateIn.getDate()} / ${dateIn.getMonth() +
+  export default {
+    components: {
+      CalendarDay,
+    },
+    data() {
+      return {
+        test: 'fdfd',
+        testEvents: [{name: 'Bier zuipen', description: 'lekker toch', location: 'de kroeg', startdate: '17 / 2 / 2018'}]
+      };
+    },
+    methods: {
+      formatDate(dateIn) {
+        return `${dateIn.getDate()} / ${dateIn.getMonth() +
         1} / ${dateIn.getFullYear()}`;
+      },
+      getMondayOfWeek(dateIn) {
+        const monday = dateIn;
+        while (monday.getDay() !== 1) {
+          monday.setDate(monday.getDate() - 1);
+        }
+        return monday;
+      },
+      getDaysOfThisWeek() {
+        const days = [];
+        const monday = this.getMondayOfWeek(new Date());
+        days.push(monday);
+        for (let i = 1; i <= 6; i += 1) {
+          // TODO: check why +1
+          const newDate = new Date(
+            monday.getFullYear(),
+            monday.getMonth(),
+            monday.getDate() + i,
+          );
+          days.push(newDate);
+        }
+        return days;
+      },
+      moveScrollDown() {
+        this.$refs.scrollablecontent.scrollTop = 480;
+      },
     },
-    getMondayOfWeek(dateIn) {
-      const monday = dateIn;
-      while (monday.getDay() !== 1) {
-        monday.setDate(monday.getDate() - 1);
-      }
-      console.log(monday);
-      return monday;
+    computed: {
+      today() {
+        return new Date();
+      },
     },
-    getDaysOfThisWeek() {
-      const days = [];
-      const monday = this.getMondayOfWeek(new Date());
-      days.push(monday);
-      for (let i = 1; i <= 6; i += 1) {
-        // TODO: check why +1
-        const newDate = new Date(
-          monday.getFullYear(),
-          monday.getMonth(),
-          monday.getDate() + i,
-        );
-        days.push(newDate);
-      }
-      return days;
+    mounted() {
+      this.moveScrollDown();
     },
-  },
-  computed: {
-    today() {
-      return new Date();
-    },
-  },
-};
+  };
 </script>
