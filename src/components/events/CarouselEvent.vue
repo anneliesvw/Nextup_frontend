63b092e4109d03ecea1998f114e80cde49ac6958
Annelies <annelies.vanwallendael@student.kdg.be>
2018-02-21 12:30:25 +0100

    fix carousel
    carousel werkt joepiee

---
+++ src/components/events/CarouselEvent.vue
@@ -0,0 +1,28 @@
+<template>
+  <carousel class="discover-carousel" :paginationEnabled="false" :navigationEnabled="true">
+          <slide class="discover-tile" v-for="i in 10" :key="i">
+            <div class="discover-tile__media">
+              <div class="discover-tile__img"></div>
+            </div>
+            <div class="discover-tile__details">
+              <div class="discover-tile__title">
+                Top Gear
+              </div>
+            </div>
+          </slide>
+        </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
--- src/containers/DiscoverContainer.vue
+++ src/containers/DiscoverContainer.vue
@@ -5,131 +5,21 @@
     <div class="discover-wrapper">
       <div class="discover-recommended">
         <div class="discover-title">recommended</div>
-        <div class="discover-contain">
-          <div class="discover-arrow left" @click="slideLeft">
-            <i class="fas fa-chevron-left"></i>
-          </div>
-          <div class="discover-row" ref="goLeft">
-            <div class="discover-row__inner">
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
-              <div class="discover-tile">
-                <div class="discover-tile__media">
-                  <div class="discover-tile__img"></div>
-                </div>
-                <div class="discover-tile__details">
-                  <div class="discover-tile__title">
-                    Top Gear
-                  </div>
-                </div>
-              </div>
+        <carousel class="discover-carousel" :perPage="1" :perPageCustom="[[768, 2], [1024, 3], [1340, 5], [1600, 6], [1900, 7]]" :paginationEnabled="false" :navigationEnabled="true">
+          <slide class="discover-tile" v-for="i in 10" :key="i">
+            <div class="discover-tile-img"></div>
+            <div class="discover-tile-title">Top Gear</div>
+            <!-- <div class="discover-tile__media">
+              <div class="discover-tile__img"></div>
             </div>
-          </div>
-
-          <div class="discover-arrow right" @click="slideRight">
-            <i class="fas fa-chevron-right"></i>
-          </div>
-        </div>
-
-        <!-- <div v-for="i in 3" :key="i">
-          <div :class="['discover-row row-' +i]">
-            <div v-for="j in 3" :key="j" class="discover-activity">
-              <div class="discover-graphic" @click="openDialog()">
-                <div class="discover-overlay">
-                  <div class="discover-overlay-name">
-                    Eventnaam
-                  </div>
-                  <div class="activity-date">
-                    27/07
-                  </div>
-                  <div class="activity-date">
-                    10:00 - 17:00
-                  </div>
-                </div>
+            <div class="discover-tile__details">
+              <div class="discover-tile__title">
+                Top Gear
               </div>
-            </div>
-          </div>
-        </div> -->
-      </div>
-      <div class="discover-recommended">
-        <div class="discover-title">others</div>
-        <div v-for="i in 3" :key="i">
-          <div :class="['discover-row row-' +i]">
-            <div v-for="j in 3" :key="j" class="discover-activity">
-              <div class="discover-graphic" @click="openDialog()">
-                <div class="discover-overlay">
-                  <div class="discover-overlay-name">
-                    Eventnaam
-                  </div>
-                  <div class="activity-date">
-                    27/07
-                  </div>
-                  <div class="activity-date">
-                    10:00 - 17:00
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            </div> -->
+          </slide>
+        </carousel>
+        
       </div>
     </div>
     <chat-menu></chat-menu>
@@ -137,46 +27,51 @@
 </template>
 
 <script>
-  import Sidebar from '../components/sidebars/Sidebar.vue';
-  import ChatMenu from '../components/sidebars/ChatMenu.vue';
-  import DetailEvent from '../components/groups/detailEvent.vue';
-  import PatternGenerator from '../services/patterngenerator';
+import { Carousel, Slide } from 'vue-carousel';
+import Sidebar from '../components/sidebars/Sidebar.vue';
+import ChatMenu from '../components/sidebars/ChatMenu.vue';
+import DetailEvent from '../components/groups/detailEvent.vue';
+import PatternGenerator from '../services/patterngenerator';
+import CarouselEvent from '../components/events/CarouselEvent.vue';
 
-  export default {
-    components: {
-      Sidebar,
-      ChatMenu,
-      DetailEvent,
-      PatternGenerator,
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Sidebar,
+    ChatMenu,
+    DetailEvent,
+    PatternGenerator,
+    CarouselEvent,
+  },
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    openDialog() {
+      this.dialogVisible = true;
     },
-    data() {
+    closeDialog() {
+      this.dialogVisible = false;
+    },
+    slideRight() {
+      const element = this.$refs.goLeft;
+      element.scrollLeft += 250;
+    },
+    slideLeft() {
+      const element = this.$refs.goLeft;
+      element.scrollLeft -= 250;
+    },
+  },
+  computed: {
+    backgroundPattern() {
+      const pattern = PatternGenerator.generateImage(`${Math.random() * 2345}`);
       return {
-        dialogVisible: false,
+        backgroundImage: pattern,
       };
     },
-    methods: {
-      openDialog() {
-        this.dialogVisible = true;
-      },
-      closeDialog() {
-        this.dialogVisible = false;
-      },
-      slideRight() {
-        const element = this.$refs.goLeft;
-        element.scrollLeft += 250;
-      },
-      slideLeft() {
-        const element = this.$refs.goLeft;
-        element.scrollLeft -= 250;
-      },
-    },
-    computed: {
-      backgroundPattern() {
-        const pattern = PatternGenerator.generateImage(`${Math.random() * 2345}`);
-        return {
-          backgroundImage: pattern,
-        };
-      },
-    },
-  };
+  },
+};
 </script>
