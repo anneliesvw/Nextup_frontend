d87fefa643fb02d1f208b3464084a82ab3cd99e1
Annelies <annelies.vanwallendael@student.kdg.be>
2018-02-27 14:07:19 +0100

    add calender item weg uit main

--- src/containers/MainContainer.vue
+++ src/containers/MainContainer.vue
@@ -3,13 +3,6 @@
     <div class="main-container">
       <Sidebar></Sidebar>
       <div class="content-container">
-        <calendar-item 
-        v-if="dialogVisible"
-        :isVisible="true"
-        @close="closeDialog"
-        >
-        </calendar-item>
-        <el-button @click="openDialog">calender item</el-button>
         <Activity v-for="item in 10" :key="item"></Activity>
       </div>
       <chat-menu></chat-menu>
@@ -21,27 +14,16 @@
 import Activity from '../components/activities/Activity.vue';
 import Sidebar from '../components/sidebars/Sidebar.vue';
 import ChatMenu from '../components/sidebars/ChatMenu.vue';
-import CalendarItem from '../components/events/CreateCalendarItem.vue';
 
 export default {
   components: {
     Activity,
     Sidebar,
     ChatMenu,
-    CalendarItem,
   },
   data() {
-    return {
-      dialogVisible: false,
-    };
+    return {};
   },
-  methods: {
-    openDialog() {
-      this.dialogVisible = true;
-    },
-    closeDialog() {
-      this.dialogVisible = false;
-    },
-  },
+  methods: {},
 };
 </script>
