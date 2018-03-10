4280bef21e68c5abea2eee77044e527f00d17fff
Annelies <annelies.vanwallendael@student.kdg.be>
2018-03-10 17:20:37 +0100

    QR test

--- src/containers/GroupDetailsContainer.vue
+++ src/containers/GroupDetailsContainer.vue
@@ -1,81 +1,51 @@
 <template>
   <div class="group-detail-container main-container">
-    <CreateEvent
-      v-if="eventDialogVisible"
-      :isVisible="true"
-      :activeGroup="activeGroup"
-      @close="closeEventDialog">
+    <CreateEvent v-if="eventDialogVisible" :isVisible="true" :activeGroup="activeGroup" @close="closeEventDialog">
     </CreateEvent>
-    <MemberDialog
-      v-if="dialogVisible"
-      :isVisible="true"
-      @close="closeDialog"
-      :activeGroup="activeGroup">
+    <MemberDialog v-if="dialogVisible" :isVisible="true" @close="closeDialog" :activeGroup="activeGroup">
     </MemberDialog>
-    <GroupDialog
-      v-if="groupDialogVisible"
-      :isVisible="true"
-      @close="closeGroupDialog">
+    <GroupDialog v-if="groupDialogVisible" :isVisible="true" @close="closeGroupDialog">
     </GroupDialog>
-    <CreatePoll
-      v-if="pollDialogVisible"
-      :activeGroup="this.activeGroup"
-      :isVisible="true"
-      @close="closePollDialog">
+    <CreatePoll v-if="pollDialogVisible" :activeGroup="this.activeGroup" :isVisible="true" @close="closePollDialog">
     </CreatePoll>
-    <PollDialog 
-      :poll="this.pollDetail"
-      v-if="pollDetailVisible"
-      :isVisible="true"
-      :activeGroup="this.activeGroup"
-      @close="closePollDetail">
+    <PollDialog :poll="this.pollDetail" v-if="pollDetailVisible" :isVisible="true" :activeGroup="this.activeGroup" @close="closePollDetail">
     </PollDialog>
 
     <div class="sidebar-wrapper">
-      <GroupSidebar
-        @groupSelected="onGroupSelected"
-        @createClicked="onCreateGroup"
-        :activeGroup="activeGroup">
+      <GroupSidebar @groupSelected="onGroupSelected" @createClicked="onCreateGroup" :activeGroup="activeGroup">
       </GroupSidebar>
     </div>
     <div class="group-wall" v-if="activeGroup">
       <banner :title="activeGroup.name" :image="bannerImage">
         <div class="banner-buttons">
-            <el-button type="primary" @click="dialogVisible = true">Invite Others</el-button> 
-            <!-- TODO: BELANGRIJK!! -->
-            <!-- Degene die leave group uitwerkt zet deze lijn code bij de onsuccess aub, de chat dankt u -->
-            <!-- this.$socket.emit('leaveroom', `${GROUPID}_${NAME}`); -->
-            <!-- met juiste waarden voor id en name -->
-            <!-- <el-button type="danger">Leave Group</el-button> -->
-            <!-- beste maker van de chat, dit zou niet mogen bepaalt worden door de frontend, is backend logica. Martin Fowler dankt u -->
-            <el-button type="danger" @click="deleteGroup">Delete Group</el-button>
-          </div>
+          <el-button type="primary" @click="dialogVisible = true">Invite Others</el-button>
+          <!-- TODO: BELANGRIJK!! -->
+          <!-- Degene die leave group uitwerkt zet deze lijn code bij de onsuccess aub, de chat dankt u -->
+          <!-- this.$socket.emit('leaveroom', `${GROUPID}_${NAME}`); -->
+          <!-- met juiste waarden voor id en name -->
+          <!-- <el-button type="danger">Leave Group</el-button> -->
+          <!-- beste maker van de chat, dit zou niet mogen bepaalt worden door de frontend, is backend logica. Martin Fowler dankt u -->
+          <el-button type="danger" @click="deleteGroup">Delete Group</el-button>
+        </div>
       </banner>
+      <qrcode-vue :value="qrvalue" :size="qrsize" level="H"></qrcode-vue>
       <div class="group-panels">
-        <InfoPanel title="Events" 
-          :events="activeGroup.events"
-          @showEventDialog="eventDialogVisible = true">
+        <InfoPanel title="Events" :events="activeGroup.events" @showEventDialog="eventDialogVisible = true">
         </InfoPanel>
-        <PollInfoPanel title="Polls" :polls="activeGroup.polls" 
-          @showPollDetail="showPollDetail($event)" 
-          @showCreatePoll="setPollDialogVisible">
+        <PollInfoPanel title="Polls" :polls="activeGroup.polls" @showPollDetail="showPollDetail($event)" @showCreatePoll="setPollDialogVisible">
         </PollInfoPanel>
       </div>
     </div>
     <div class="no-group-selected" v-else>
-      <EmptyState
-        size="large"
-        title="No Group Selected"
-        text="please select a group through the menu on the left side"
-        icon="hand-point-left"
-      >
+      <EmptyState size="large" title="No Group Selected" text="please select a group through the menu on the left side" icon="hand-point-left">
 
       </EmptyState>
     </div>
-  </div>  
+  </div>
 </template>
 
 <script>
+import QrcodeVue from 'qrcode.vue';
 import GroupSidebar from '../components/sidebars/GroupSidebar.vue';
 import MemberDialog from '../components/groups/MemberDialog.vue';
 import InfoPanel from '../components/groups/InfoPanel.vue';
@@ -90,6 +60,7 @@ import Banner from '../components/layout_misc/Banner.vue';
 
 export default {
   components: {
+    QrcodeVue,
     GroupSidebar,
     InfoPanel,
     MemberDialog,
@@ -122,6 +93,8 @@ export default {
       pollDialogVisible: false,
       pollDetailVisible: false,
       pollDetail: '',
+      qrvalue: 'www.google.be',
+      size: 300,
     };
   },
   methods: {
