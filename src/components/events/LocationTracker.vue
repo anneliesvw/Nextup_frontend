ad0b74e9f700ef462303dccb4ddefc8cf3202e3d
Mahen Vermeulen <mahenvermeulen@msn.com>
2018-03-10 01:25:17 +0100

    LocationTracker files

---
+++ src/components/events/LocationTracker.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="location-tracker">
+    <div class="location-map">
+      <div 
+        v-show="currentUser"
+        ref="titledialog" 
+        class="current-user" 
+        >
+        {{currentUser ? currentUser.person.firstName : ''}}
+      </div>
+      <gmap-map :center="eventLocation" :zoom="7" style="">
+        <gmap-marker v-for="m in markers" 
+          :position="m.position" 
+          :clickable="true"
+          :key="m.id"
+          :draggable="false" 
+          @mouseover="(e) => showTitle(e, m.user)"
+          @mouseout="hideTitle"
+          :optimized="false"
+          >
+        </gmap-marker>
+      </gmap-map>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        currentUser: null,
+        eventLocation: {
+          lat: 50.0,
+          lng: 50.0,
+        },
+        locations: [
+          {
+            id: 1,
+            location: {
+              longitude: 51.00001,
+              latitude: 50.02,
+            },
+            user: {
+              username: 'mahenvermeulen@msn.com',
+              person: {
+                firstName: 'mahen',
+                lastName: 'vermeulen',
+              },
+            },
+          },
+          {
+            id: 2,
+            location: {
+              longitude: 50.01,
+              latitude: 50.4000002,
+            },
+            user: {
+              username: 'mahenvermeulen@msn.com',
+              person: {
+                firstName: 'annelies',
+                lastName: 'van wallendael',
+              },
+            },
+          },
+          {
+            id: 3,
+            location: {
+              longitude: 50.04,
+              latitude: 50.0000005,
+            },
+            user: {
+              username: 'mahenvermeulen@msn.com',
+              person: {
+                firstName: 'matthias',
+                lastName: 'goossens',
+              },
+            },
+          },
+        ],
+      };
+    },
+    computed: {
+      markers() {
+        return this.locations.map(l => ({
+          user: l.user,
+          id: l.id,
+          position: {
+            lat: l.location.longitude,
+            lng: l.location.latitude,
+          },
+        }));
+      },
+    },
+    methods: {
+      showTitle(e, u) {
+        const dialog = this.$refs.titledialog;
+        this.currentUser = u;
+        dialog.style.top = `${e.Ga.clientY}px`;
+        dialog.style.left = `${e.Ga.clientX}px`;
+      },
+      hideTitle() {
+        this.currentUser = null;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
