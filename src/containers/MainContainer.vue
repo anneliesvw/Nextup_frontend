3de9e4ddbbd3bc261535c0f8b5f3580b77ba77f2
matthiasgoossens <matthias.goossens@hotmail.com>
2018-03-11 20:26:57 +0100

    Frontpage v1.O

--- src/containers/MainContainer.vue
+++ src/containers/MainContainer.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="main-container">
       <Sidebar></Sidebar>
-      <div class="content-container">
+      <div class="dashboard-container">
         <!--calendar-list></calendar-list-->
-        <location-tracker></location-tracker>
+        <!-- <location-tracker></location-tracker> -->
+        <generic-title>Upcoming Events</generic-title>
+        <div class="dashboard-activities">
+          <Activity 
+            class="dashboard-activity"
+            v-for="event in events" 
+            :key="event.eventId"
+            :event="event">
+          </Activity>
+        </div>
       </div>
     </div>
 </template>
@@ -13,6 +22,7 @@ import LocationTracker from '../components/events/LocationTracker.vue';
 import Activity from '../components/activities/Activity.vue';
 import Sidebar from '../components/sidebars/Sidebar.vue';
 import CalendarList from '../components/activities/calendar/CalendarList.vue';
+import GenericTitle from '../components/layout_misc/GenericTitle.vue';
 
 export default {
   components: {
@@ -20,6 +30,13 @@ export default {
     Sidebar,
     CalendarList,
     LocationTracker,
+    GenericTitle,
+  },
+  computed: {
+    events() {
+      const events = this.$store.getters.getGroupEvents;
+      return events.sort((a, b) => a.startDate > b.startDate);
+    },
   },
 };
 </script>
